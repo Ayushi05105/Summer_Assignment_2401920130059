5e684677c6a8eff85549f77192fd503fc84c0f2e
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-02 14:43:40 +0530

    updated code

---
+++ WEEK1/DAY2/maxAverageSubarray.java
@@ -0,0 +1,23 @@
+package WEEK1.DAY2;
+
+public class maxAverageSubarray {
+    public static double findMaxAverage(int[] nums, int k) {
+        int sum =0;
+        for(int i =0;i<k;i++){
+            sum += nums[i];
+        }
+        int max = sum;
+        for(int i = k;i<nums.length;i++){
+            sum += nums[i];
+            sum -= nums[i-k];
+            max = Math.max(max,sum);
+        }
+        return (double) max/k;
+    }
+    public static void main(String[] args) {
+        int[] nums ={1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(findMaxAverage(nums, k));
+    }
+    
+}
