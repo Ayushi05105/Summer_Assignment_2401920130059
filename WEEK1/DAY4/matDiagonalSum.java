c0f0fc1d5a408a6d79545fc845dcaae6b29dfc41
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-04 13:45:36 +0530

    updated code

---
+++ WEEK1/DAY4/matDiagonalSum.java
@@ -0,0 +1,21 @@
+package WEEK1.DAY4;
+public class matDiagonalSum {
+
+     public static int diagonalSum(int[][] mat) {
+       int n = mat.length;
+        int sum =0;
+        for(int i=0;i<n;i++){
+            sum += mat[i][i];
+            sum += mat[i][n-i-1];
+        }
+        if(n %2 ==1){
+            sum -=mat[n/2][n/2];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int[][] mat ={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(diagonalSum(mat));
+    }
+    
+}
