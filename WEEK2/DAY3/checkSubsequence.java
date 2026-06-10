c3d726e6c8e73eddf2606eca29658b71ee1d34d5
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-10 14:54:40 +0530

    updated code

---
+++ WEEK2/DAY3/checkSubsequence.java
@@ -0,0 +1,24 @@
+package WEEK2.DAY3;
+
+public class checkSubsequence {
+
+    public static boolean isSubsequence(String s, String t) {
+        int i =0;
+        int j =0;
+        while(i<s.length() && j< t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+            i++;
+            }
+            j++;
+        }
+        
+        return i== s.length();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 ="ahbgdc";
+        System.out.println(isSubsequence(s1, s2));
+    }
+    
+}
