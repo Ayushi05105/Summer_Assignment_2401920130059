1294ba958b9c67d684660f4f2b3a1615d5722906
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-11 16:17:08 +0530

    updated code

---
+++ WEEK2/DAY4/reversewordInString.java
@@ -0,0 +1,31 @@
+package WEEK2.DAY4;
+
+public class reversewordInString {
+    public static void reverse(char[] arr,int left,int right){
+        while(left<right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public static String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+        int start =0;
+        for(int end = 0;end<=arr.length;end++){
+            if(end ==arr.length || arr[end] ==' '){
+                reverse(arr,start,end-1);
+                start = end+1;
+            }
+        }
+        return new String(arr);
+
+    }
+
+    public static void main(String[] args) {
+        String s ="Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+    
+}
