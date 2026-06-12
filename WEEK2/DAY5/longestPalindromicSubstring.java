d7f2153e498632f4383d5cf2e50d0df30e39ebac
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-12 23:10:00 +0530

    updated code

---
+++ WEEK2/DAY5/longestPalindromicSubstring.java
@@ -0,0 +1,40 @@
+package WEEK2.DAY5;
+
+public class longestPalindromicSubstring {
+
+    public static String longestPalindrome(String s) {
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+
+                String sub = s.substring(i, j + 1);
+
+                if (isPalindrome(sub) && sub.length() > ans.length()) {
+                    ans = sub;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private static boolean isPalindrome(String str) {
+        int l = 0, r = str.length() - 1;
+
+        while (l < r) {
+            if (str.charAt(l) != str.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String s ="babad";
+        System.out.println(longestPalindrome(s));
+    }
+    
+}
