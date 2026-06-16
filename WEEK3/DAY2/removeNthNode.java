d59e93827759cbd612651a0d2d5e392f9b3bc8d6
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-16 22:43:12 +0530

    updated code

--- WEEK3/DAY2/removeNthNode.java
+++ WEEK3/DAY2/removeNthNode.java
@@ -1,8 +1,17 @@
 package WEEK3.DAY2;
 
-import WEEK3.DAY2.mergeLL.ListNode;
 
 public class removeNthNode {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
      ListNode temp = new ListNode(0);
@@ -22,5 +31,25 @@ public class removeNthNode {
      return temp.next;
 
     }
+
+    public static void main(String[] args) {
+
+    removeNthNode obj = new removeNthNode();
+
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+
+    int n = 2;
+
+    ListNode result = obj.removeNthFromEnd(head, n);
+
+    while (result != null) {
+        System.out.print(result.val + " ");
+        result = result.next;
+    }
+}
     
 }
