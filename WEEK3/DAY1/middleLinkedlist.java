1f30c76a3fd105a2f8882294ab68e8d476effa74
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-16 22:41:01 +0530

    updated code

--- WEEK3/DAY1/middleLinkedlist.java
+++ WEEK3/DAY1/middleLinkedlist.java
@@ -2,6 +2,38 @@ package WEEK3.DAY1;
 
 public class middleLinkedlist {
 
-    
-    
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        middleLinkedlist obj = new middleLinkedlist();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode middle = obj.middleNode(head);
+        System.out.println("Middle Node = " + middle.val);
+        
+    }
+
 }
+    
+
